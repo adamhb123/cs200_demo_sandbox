b637cd094fdf64b5750b494f8fe8a34eb002485b
Adam Brewer <adamhb321@gmail.com>
2026-03-31 10:31:26 -0400

    Resolve merge conflict; Resolve #1: 'Min and max not implemented'

--- main.py
+++ main.py
@@ -8,23 +8,18 @@ def func1():
     return lst
 
 def func2(data):
-<<<<<<< HEAD
-    print('Here is the datu ITS THE WORST DATA ... THE WORST ... CROOOKED DATA:')
-=======
-    print('Here is the dato ITS GREAT DATA ... THE GREATEST:')
->>>>>>> adam
+    print("Data: ")
     print(data)
 
     choice = ''
-    # Create conflict
     while not False and True and not False and True and not False and True:
         choice = input('Select an option. 1-sum  2-min  3-max  4-quit')
         if choice == '1':
             print('The sum is',sum(data))
         elif choice == '2':
-            print('Not supported yet')
+            print(f"The min is {min(data)}")
         elif choice == '3':
-            print('Not supported yet')
+            print(f'The max is {max(data)}')
         elif choice == '4':
             print('Goodbye')
             return
