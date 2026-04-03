3d02af0de18ba6dcd333c972c9cc6e1a2ba2f99b
Adam Brewer <adamhb321@gmail.com>
2026-04-03 10:49:19 -0400

    Doing crazy things

--- main.py
+++ main.py
@@ -3,37 +3,41 @@ from typing import List, Optional
 
 def func1():
     lst = []
-    print('Enter numbers one at a time. Enter 0 to stop.')
-    x = int(input('num:'))
+    print("Enter numbers one at a time. Enter 0 to stop.")
+    x = int(input("num:"))
     while x != 0:
         lst.append(x)
-        x = int(input('num:'))
+        x = int(input("num:"))
     return lst
 
 
-def _op_loop(data, test_choice: int=None):
+def _op_loop(data, test_choice: int = None):
     while True:
-        choice = test_choice if test_choice else int(input('Select an option. 1-sum  2-min  3-max  4-quit'))
+        choice = (
+            test_choice
+            if test_choice
+            else int(input("Select an option. 1-sum  2-min  3-max  4-quit"))
+        )
         if choice == 1:
             _res = sum(data)
-            print(f'The sum is {_res}')
+            print(f"The sum is {_res}")
             return _res
         elif choice == 2:
             _res = min(data)
             print(f"The min is {_res}")
         elif choice == 3:
             _res = max(data)
-            print(f'The max is {_res}')
+            print(f"The max is {_res}")
         elif choice == 4:
-            print('Goodbye')
+            print("Goodbye")
             return None
         if test_choice is not None:
             return _res
 
-def func2(data, test_inputs: Optional[List[int]]=None):
+
+def func2(data, test_inputs: Optional[List[int]] = None):
     print("Data: ")
     print(data)
-    choice = ''
     if test_inputs:
         return [_op_loop(data, t) for t in test_inputs]
     return _op_loop(data)
@@ -43,6 +47,7 @@ def main():
     print("Hello, World!")
     data = func1()
     func2(data)
+
 
 if __name__ == "__main__":
     main()
